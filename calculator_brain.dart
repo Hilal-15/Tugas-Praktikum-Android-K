3093734446ecf8e14659bf9bb22656308179f957
Hilal-15 <93043505+Hilal-15@users.noreply.github.com>
2021-11-19 03:59:50 +0800

    Add files via upload

---
+++ calculator_brain.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain({this.height, this.weight});
+
+  final int weight;
+  final int height;
+  double _bmi;
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'KEGEMUKAN';
+    } else if (_bmi > 18.5) {
+      return 'NORMAL';
+    } else {
+      return 'TERLALU KURUS';
+    }
+  }
+
+  String getInterPretation() {
+    if (_bmi >= 25) {
+      return 'Anda Memiliki Berat Badan Yang Lebih Tinggi Dari Biasanya. Cobalah Berolahraga.';
+    } else if (_bmi > 18.5) {
+      return 'Anda Memiliki Berat Badan Nornal. Pertahankan Itu.';
+    } else {
+      return 'Anda Memiliki Berat Badan Lebih Rendah dari Mormal. Cobalah Makan Lenih Banyak';
+    }
+  }
+}
---
+++ result_page.dart
@@ -0,0 +1,80 @@
+import 'package:bmi_calculator_praktikum_k/components/bottom_button.dart';
+import 'package:bmi_calculator_praktikum_k/components/reusable_card.dart';
+import 'package:bmi_calculator_praktikum_k/constant.dart';
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  static const routeName = '/result';
+  const ResultPage({this.bmiResult, this.resultText, this.InterPretation});
+
+  final String bmiResult;
+  final String resultText;
+  final String InterPretation;
+
+  @override
+  Widget build(BuildContext context) {
+    final ResultPage resultPage = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomLeft,
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Hasil Anda Adalah',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        resultPage.resultText,
+                        style: kResultTextStyle,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        resultPage.bmiResult,
+                        style: kBMITextStyle,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        resultPage.InterPretation,
+                        style: kBodyTextStyle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+              ButtonText: 'HITUNG ULANG',
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              })
+        ],
+      ),
+    );
+  }
+}
